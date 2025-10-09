56a621f61942dc6c568e4a273b0182bc3b5288b0
Ankita Patel <ankitapatel11068@gmail.com>
2025-10-09 21:43:49 +0530

    added

---
+++ 29-july.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+// Ques-1: What is deep and shallow copy please explain with example?
+
+
+
+
+
+
+
+// Ques-2: print this start pattern?
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+// Ques-3: Create an person object which have some property. 
+// name, age, mobile , gender, address :[streetNumber, pincode], hobbies - should be an array of string = ['reading','learning','watching-movie']
+//
+
+
+
+
+
+// Ques-4: What is Object ?
+
+
+// Ques-5: What is variable?
+
+// Ques-6: Please explain about let , var & const?
+
+
+
+
+
+
+
